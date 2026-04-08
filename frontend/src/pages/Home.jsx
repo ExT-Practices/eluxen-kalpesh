aa7967668d387b944e9295a527d999ef504d3bb9
KALPESH PRAJAPATI <kp483368@gmail.com>
2026-04-08 14:28:39 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import AboutUs from "../components/AboutUs";
+import Service from "../components/Service";
+import Pricing from "../components/Pricing";
+import Testimonials from "../components/Testimonials";
+import FAQ from "../components/FAQ";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+
+export default function Home() {
+    return (
+        <>
+            <Header />
+            <main className="relative">
+                <Navbar />
+                <Hero />
+                <Service />
+                <AboutUs />
+                <Pricing />
+                <Testimonials />
+                <FAQ />
+                <Contact />
+                <Footer />
+            </main>
+        </>
+    );
+}
