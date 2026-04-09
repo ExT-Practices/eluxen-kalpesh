d4994c45e86dd6c120736e5c85d3101f628dd029
KALPESH PRAJAPATI <kp483368@gmail.com>
2026-04-09 14:20:48 +0530

    Create FAQ.js

---
+++ backend/models/FAQ.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const faqSchema = new mongoose.Schema({
+    question: String,
+    answer: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('FAQ', faqSchema);
